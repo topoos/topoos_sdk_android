7c601a9960ac47de4e495e6d03979c5d27728173
mjgs <mgarcia@solusoft.es>
2014-03-17 12:40:08 +0100

    License Apache

--- src/topoos/APIAccess/Operations/POISWarningDelete.java
+++ src/topoos/APIAccess/Operations/POISWarningDelete.java
@@ -1,6 +1,21 @@
+/**
+ * Copyright 2014-present topoos
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package topoos.APIAccess.Operations;
 
-// TODO: Auto-generated Javadoc
 /**
  * Delete all error warnings that are registered in a POI, the category has to
  * belong to the application.
