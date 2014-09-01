2bb8fd900fef4e992f2124b040301ccfe2ed4da7
DMC <dmoreno@solusoft.es>
2014-09-01 13:23:57 +0200

    Configuration official URL Topoos

--- src/topoos/Constants.java
+++ src/topoos/Constants.java
@@ -20,14 +20,14 @@ public class Constants {
 	public static boolean DEBUG = false;
 	public static boolean DEBUGURL = true;
 	
-//	public static final String TOPOOSURIPIC="https://pic.topoos.com";
-//	public static final String TOPOOSURIAPI="https://api.topoos.com";
-//	public static final String TOPOOSURILOGIN="https://login.topoos.com";
-//	public static final String TOPOOSURISOCIAL="https://social.topoos.com";
-	public static final String TOPOOSURIPIC="http://tos-pre.cloudapp.net:9003";
-	public static final String TOPOOSURIAPI="http://tos-pre.cloudapp.net:9000";
-	public static final String TOPOOSURILOGIN="http://tos-pre.cloudapp.net:9001";
-	public static final String TOPOOSURISOCIAL="http://tos-pre.cloudapp.net:9002";	
+	public static final String TOPOOSURIPIC="https://pic.topoos.com";
+	public static final String TOPOOSURIAPI="https://api.topoos.com";
+	public static final String TOPOOSURILOGIN="https://login.topoos.com";
+	public static final String TOPOOSURISOCIAL="https://social.topoos.com";
+//	public static final String TOPOOSURIPIC="http://tos-pre.cloudapp.net:9003";
+//	public static final String TOPOOSURIAPI="http://tos-pre.cloudapp.net:9000";
+//	public static final String TOPOOSURILOGIN="http://tos-pre.cloudapp.net:9001";
+//	public static final String TOPOOSURISOCIAL="http://tos-pre.cloudapp.net:9002";	
 	public static final String TAG="SDK_TOPOOS";
 	public static final int HTTP_WAITING_MILISECONDS = 5000;
 	public static final String SDKVERSION = "1.0.0.12";
